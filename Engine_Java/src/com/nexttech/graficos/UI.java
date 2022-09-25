d7d1157bce04f2a3b5ac9da9a380f1b08eab8188
Jan Eduardo <janeduardo@discente.ufg.br>
2022-09-25 01:42:29 -0300

    cleaning engine to start second game and adding a default project just in case

---
+++ Engine_Java/src/com/nexttech/graficos/UI.java
@@ -0,0 +1,12 @@
+package com.nexttech.graficos;
+
+
+import java.awt.Graphics;
+
+public class UI {
+
+	public void render(Graphics g) {
+		
+	}
+	
+}
---
+++ Game_02/src/com/nexttech/entities/Player.java
@@ -0,0 +1,31 @@
+package com.nexttech.entities;
+
+import java.awt.image.BufferedImage;
+
+import com.nexttech.world.World;
+
+public class Player extends Entity{
+	
+	public boolean right,up,left,down;
+
+	public Player(int x, int y, int width, int height,double speed,BufferedImage sprite) {
+		super(x, y, width, height,speed,sprite);
+	}
+	
+	public void tick(){
+		depth = 1;
+		if(right && World.isFree((int)(x+speed),this.getY())) {
+			x+=speed;
+		}
+		else if(left && World.isFree((int)(x-speed),this.getY())) {
+			x-=speed;
+		}
+		if(up && World.isFree(this.getX(),(int)(y-speed))){
+			y-=speed;
+		}
+		else if(down && World.isFree(this.getX(),(int)(y+speed))){
+			y+=speed;
+		}
+	}
+
+}
